4349d2534dd8df8ed9d43a255b6301c062cd4754
Ezell <lynellf@gmail.com>
2018-01-23 20:19:43 -0600

    still tweaking the charts

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // Step Three (a): using chart.js create and include the information for the following chart widgets as shown in the mockup for the 1. web traffic
 
-const trafficDiv = document.querySelector('#traffic');
+const trafficDiv = document.querySelector('#trafficLine'),
+  trafficDaily = document.querySelector('#trafficBar'),
+  trafficMobile = document.querySelector('#trafficPie');
 
-let myChart = new Chart (trafficDiv, {
+let siteTraffic = new Chart (trafficDiv, {
     type: 'line',
     data: {
         labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
@@ -16,8 +18,51 @@ let myChart = new Chart (trafficDiv, {
     options: {
       title: {
         display: false,
-        text: 'Traffic'
+        text: 'Traffic',
+        responsive: true,
+        maintainAspectRatio: false,
       }
     }
 }
+}),
+
+// Bar chart
+dailyTraffic = new Chart(trafficDaily, {
+  type: 'bar',
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [
+      {
+        backgroundColor: "#4D4B72",
+        data: [75, 105, 165, 125, 225, 198, 98]
+      }
+    ]
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true
+    }
+  }
+}),
+
+mobileTraffic = new Chart(trafficMobile, {
+  type: 'pie',
+  data: {
+    labels: ["Phones", "Tablets", "Desktop"],
+    datasets: [{
+      backgroundColor: ["#4D4B72", "green", "blue"],
+      data: [75, 25, 25]
+    }]
+  },
+  options: {
+    legend: {
+      display: true,
+      position: "right"
+    }
+  }
 });
+
+
+
+
